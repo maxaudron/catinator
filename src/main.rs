28c29a85309dc40b567df66638a4472ea2d1507f
R0flcopt3r <12752060+R0flcopt3r@users.noreply.github.com>
2021-10-09 16:07:09 +0200

    for running test shit

--- src/main.rs
+++ src/main.rs
@@ -19,44 +19,10 @@ async fn main() {
             catinator::hooks::sasl
         ),
         hook(
-            "sed_log",
-            "Log messages for use with sed replace, max 10k lines.",
-            PRIVMSG,
-            sed.log
-        ),
-        hook(
             "url_preview",
             "Send preview of website",
             PRIVMSG,
             catinator::hooks::url::url_preview
         )
-        matcher(
-            "shifty_eyes",
-            ">.>",
-            r"^\S{3}$",
-            catinator::hooks::shifty_eyes
-        ),
-        matcher(
-            "replace",
-            "sed style replace with regex support. i/g/U/x sed flags available",
-            r"^s/",
-            sed.replace
-        ),
-        matcher(
-            "intensify",
-            "makes everything kinda more intense",
-            r"^\[.*?\]$",
-            catinator::hooks::intensify
-        ),
-        command(
-            "pet",
-            "Pet the cat, cats generally like pets.",
-            catinator::hooks::pet::pet
-        ),
-        command(
-            "about",
-            "Prints some info about this kitty cat",
-            catinator::hooks::about
-        ),
     ];
 }
