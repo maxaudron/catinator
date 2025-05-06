daf361ea763de47d61860df75a1b8c9a88aa17eb
Max Audron <audron@cocaine.farm>
2025-05-06 16:33:31 +0200

    fix truncate on unicode
    fuck unicode all my homies stan ascii

--- src/util/formatting/truncate.rs
+++ src/util/formatting/truncate.rs
@@ -1,3 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 /// Truncates a string after a certain number of characters.
 ///
 /// Function always tries to truncate on a word boundary.
@@ -6,15 +8,7 @@ pub fn truncate(text: &str, len: usize) -> String {
     if text.len() <= len {
         return text.to_string();
     }
-    format!(
-        "{}…",
-        text[..len]
-            .rsplitn(2, " ")
-            .collect::<Vec<&str>>()
-            .last()
-            .copied()
-            .expect("This can never happen >inb4 it happens")
-    )
+    format!("{}…", text.graphemes(true).take(len).collect::<String>())
 }
 
 #[cfg(test)]
@@ -39,7 +33,7 @@ mod tests {
     fn test_truncate_with_input_of_greater_length_than_limit() {
         let input = "some longer text";
         let result = truncate(input, input.len() - 1);
-        assert_eq!("some longer…", result)
+        assert_eq!("some longer tex…", result)
     }
 
     #[test]
@@ -47,5 +41,12 @@ mod tests {
         let input = "somelongertext";
         let result = truncate(input, input.len() - 1);
         assert_eq!("somelongertex…", result)
+    }
+
+    #[test]
+    fn test_truncate_with_unicode() {
+        let input = "short ° text";
+        let result = truncate(input, 7);
+        assert_eq!(result, "short °…")
     }
 }
